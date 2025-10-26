d30b25b6ed2322f9d87ab508a15cf3aaa6863115
Hoang Hung <hhungln28@gmail.com>
2025-10-26 14:53:02 +0700

    some adjusted in subBytes

--- subBytes.v
+++ subBytes.v
@@ -3,11 +3,11 @@ module subBytes(in, out);
 input   [127:0] in;
 output  [127:0] out;
 
-genvar i;
-generate
-    for(i = 0; i < 128; i++) begin: sub_bytes
-    sBox s(in[i +:8], out[i +:8]);
-    end
-endgenerate
+    genvar i;
+    generate
+        for(i = 0; i < 128; i++) begin: sub_bytes
+        sBox s(in[i +:8], out[i +:8]);
+        end
+    endgenerate
 
 endmodule
